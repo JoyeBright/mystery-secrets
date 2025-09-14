47977cc076022cbe36d840abfa1d2d672df29b99
Javad Pourmostafa <j.pourmostafa@tilburguniversity.edu>
2025-09-14 14:56:39 +0200

    add ip inventory

--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
 # 3 top secrets, handed out one by one
 secrets = ["Captain Jack Snackrow", "Department of Intelligent Systems", "R&MD"]
 
-# Track which IPs already claimed a secret
-claimed_ips = {}
+# File to track claimed IPs
+CLAIMED_FILE = "claimed_ips.txt"
+
+# Load claimed IPs at startup
+if os.path.exists(CLAIMED_FILE):
+    with open(CLAIMED_FILE, "r") as f:
+        claimed_ips = dict(line.strip().split("=", 1) for line in f if "=" in line)
+else:
+    claimed_ips = {}
 
 @app.route("/secret")
 def get_secret():
-    client_ip = request.remote_addr
+    client_ip = request.headers.get("X-Forwarded-For", request.remote_addr)
 
     # If this IP has already claimed a secret
     if client_ip in claimed_ips:
@@ -22,6 +30,11 @@ def get_secret():
     if secrets:
         secret = secrets.pop(0)
         claimed_ips[client_ip] = secret
+
+        # Save claim to file
+        with open(CLAIMED_FILE, "a") as f:
+            f.write(f"{client_ip}={secret}\n")
+
         return jsonify({
             "ip": client_ip,
             "secret": secret
